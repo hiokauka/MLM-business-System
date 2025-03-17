93d7c32e713f6e730f92bd91cb3010a45714a5d2
Rauf Hazim <raufhazim07@gmail.com>
2025-03-17 11:27:35 +0800

    Fixed vite build error for Vercel 4 try

--- sistem/vite.config.js
+++ sistem/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: 'dist'  // This is where Vercel expects the build output
+    outDir: 'dist'
   },
-  plugins: [react()]
+  esbuild: {
+    // ✅ Ignore warning in production build
+    logLevel: 'silent'
+  }
 })
